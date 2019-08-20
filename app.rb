008b38e784a6815ca0ce64e31e4a9639d6ad235b
Keith Isham <kisham8899@gmail.com>
2019-08-20 11:13:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "While this is I guess an app, it really isnt anything but connecting to a local server and pasting some text. But Yay? "
   end
 
 end
